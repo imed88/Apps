474d167847970b9bb860c04cc18d0e9c246af0be
benzarti imed eddine <benzarti imed eddine@LAPTOP-HHFAD38D>
2020-07-18 18:00:06 +0200

    Add Delete Edit Users

--- Apps/Controllers/AccountController.cs
+++ Apps/Controllers/AccountController.cs
@@ -236,7 +236,10 @@ namespace Apps.Controllers
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
-
+                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("Index", "Administration");
+                    }
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
---
+++ Apps/Controllers/AdministrationAccount/AdministrationController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apps.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apps.Controllers.AdministrationAccount
+{
+    public class AdministrationController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdministrationController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // GET: Administration
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = userManager.Users;
+            return View(users);
+        }
+
+        // GET: Administration/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Administration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administration/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administration/Edit/5
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            // GetClaimsAsync retunrs the list of user Claims
+            var userClaims = await userManager.GetClaimsAsync(user);
+            // GetRolesAsync returns the list of user Roles
+            var userRoles = await userManager.GetRolesAsync(user);
+            var model = new ApplicationUser
+            {
+                
+               
+                Email=user.Email,
+                UserName=user.UserName,
+                Address=user.Address,
+                DOB=user.DOB
+            };
+            return View(model);
+        }
+
+        // POST: Administration/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(ApplicationUser model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.Address = model.Address;
+                user.DOB = model.DOB;
+
+                var result = await userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
+        // GET: Administration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Administration/Delete/5
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("Index");
+            }
+        }
+    }
+}
